c84859ce9a951ea24c976deff37c2bfa46b00fed
Dragon <ExplodingFKL@gmail.com>
2023-02-13 12:02:07 +0800

    feat: 修复一些问题

--- packages/Packages.mk
+++ packages/Packages.mk
@@ -5,7 +5,7 @@ RPM_OUTPUT:=$(abspath $(BINARY_DIR)/pkg/rpm)
 RPM_TOOL_DEFAULT_PARAMS:=bash $(TOOL_RPM) build --output $(RPM_OUTPUT)  --auto --debug \
 --local-repository $(RPM_OUTPUT)
 
-CONTAINER_PKG_ARGS:=(test -d $(CACHE_DIR)/go || mkdir -p $(CACHE_DIR)/go) && $(CMD_DOCKER) run -it --rm --name el7-builder -v $(CACHE_DIR)/go:/root/go \
+CONTAINER_PKG_ARGS:=(test -d $(CACHE_DIR)/go || mkdir -p $(CACHE_DIR)/go) && $(CMD_DOCKER) run -it --rm  -v $(CACHE_DIR)/go:/root/go \
   -v $(SRC_DIR):/workspace --workdir /workspace --env IN_CONTAINER=true
 
 ifeq ($(IN_CONTAINER), true)
@@ -18,7 +18,7 @@ endif
 pkg/all: pkg/rpm/el7
 
 pkg/rpm/el7: img/builder/rpm/el7
-	 $(CONTAINER_PKG_ARGS) $(DOMAIN)/builder/rpm:el7 sh -c "make pkg/setup/rpm && make pkg/rpm"
+	 $(CONTAINER_PKG_ARGS) --name el7-builder $(DOMAIN)/builder/rpm:el7 sh -c "make pkg/setup/rpm && make pkg/rpm"
 
 pkg-rpm-el7-dev: img/builder/rpm/el7
-	$(CONTAINER_PKG_ARGS) $(DOMAIN)/builder/rpm:el7 bash
+	$(CONTAINER_PKG_ARGS) --name el7-test $(DOMAIN)/builder/rpm:el7 bash
