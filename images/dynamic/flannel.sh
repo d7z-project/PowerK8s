ebb249666a8478eae7daf1cc77103a737e7d024c
Dragon <ExplodingFKL@gmail.com>
2023-02-15 17:32:47 +0800

    feat: 变更编译方案

--- images/dynamic/flannel.sh
+++ images/dynamic/flannel.sh
@@ -4,6 +4,6 @@ set -e
 ##DependsTarget=pkg/rpm/el7
 make pkg/repos/force-install
 yum install helm -y
-echo "$OCI_IMAGE_OUTPUT"
-echo >"$OCI_IMAGE_OUTPUT"
+helm template cni-flannel helm/cni-flannel --set registry=docker.io | grep "image:" | awk '{print $2}' | sort -u > "$OCI_IMAGE_OUTPUT"
+
 exit 0
